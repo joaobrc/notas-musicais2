1fa62a42d3942ee5e33a36c696d193a35e486d11
Joao Costa <joao.costa@duartecon.com.br>
2023-06-15 14:36:18 -0300

    sexto commit adicionando paramentro, retunr e exemplos a documentação

--- notas_musicais2/escalas.py
+++ notas_musicais2/escalas.py
@@ -2,21 +2,24 @@ NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
 ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
 
 
-def escala(tonica: str, tonalidade: str):
+def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]: 
     """
     Gera uma escala apartir de uma tonica  e uma tonalidade
-
 
     Parameters:
         tonica: Nota que sera a tonica da escala
         tonalidade: Tonalidade da escala
 
-    >>> escala('C', 'maior')
-    {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+    Returns:
+        Um dicionario com as informações da nota e seus graus, retonados de cada nota informado na escala
+        
+    Examples:
+        >>> escala('C', 'maior')
+        {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
 
 
-    >>> escala('B', 'maior')
-    {'notas': ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+        >>> escala('B', 'maior')
+        {'notas': ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
     """
 
     intervalos = ESCALAS[tonalidade]
